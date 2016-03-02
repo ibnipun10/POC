1da9863874ec345434c6db0e4bc99c02a049273e
nipun agarwal <ibnipun10@gmail.com>
2016-03-02 04:23:17 +0000

    updating the job

--- kinesis_python_spark_stream/kinesisStream.py
+++ kinesis_python_spark_stream/kinesisStream.py
@@ -35,7 +35,7 @@ REDSHIFT_HOSTNAME = 'venkat-test.cfxcbauz3avq.us-east-1.redshift.amazonaws.com'
 REDSHIFT_PORT = '5439'
 REDSHIFT_USERNAME = 'venkattest'
 REDSHIFT_PASSWORD = 'BPOahslA9ytWRivguhkV'
-REDSHIFT_DATABASE = 'Venkat-test'
+REDSHIFT_DATABASE = 'venkattest'
 REDSHIFT_PAGEVIEW_TBL = 'PageView'
 
 #kinesis cred
@@ -49,10 +49,10 @@ AWSACCESSID = 'AKIAJSK34E5YQK36DMRQ'
 AWSSECRETKEY = '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'
 
 #S3 cred
-S3ACCESSID = 'AKIAJCAETS5FY6O5FBJQ'
-S3SECRETKEY = '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'
-BUCKET = 'sams-staging'
-FOLDER = 'wfspoc'
+S3ACCESSID = 'AKIAJSK34E5YQK36DMRQ'
+S3SECRETKEY = '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'
+BUCKET = 'sams-analytics-poc'
+FOLDER = 'sams-poc'
 	
 def WriteToTable(df, type):
 	if type in REDSHIFT_PAGEVIEW_TBL:
@@ -63,12 +63,12 @@ def WriteToTable(df, type):
 		
 		url = ("jdbc:redshift://" + REDSHIFT_HOSTNAME + ":" + REDSHIFT_PORT + "/" +   REDSHIFT_DATABASE + "?user=" + REDSHIFT_USERNAME + "&password="+ REDSHIFT_PASSWORD)
 		
-		s3Dir = 's3://' + AWSACCESSID + ':' + AWSSECRETKEY + '@' + BUCKET + '/' + FOLDER
+		s3Dir = 's3n://' + AWSACCESSID + ':' + AWSSECRETKEY + '@' + BUCKET + '/' + FOLDER
 		
-		print url
-		print 's3 url : ', s3Dir
-		
+		print 'Start writing to redshift'
 		df.write.format("com.databricks.spark.redshift").option("url", url).option("dbtable", REDSHIFT_PAGEVIEW_TBL).option('tempdir', s3Dir).mode('Append').save()
+		
+		print 'Finished writing to redshift'
 
 def getSqlContextInstance(sparkContext):
 	if ('sqlContextSingletonInstance' not in globals()):
@@ -77,7 +77,6 @@ def getSqlContextInstance(sparkContext):
 
 def getBrowser(userAgent):
 	# check for empty or null uri
-	print userAgent
 	if userAgent:
 		browser =  re.search(r'\w+/', userAgent)
 		if browser is not None:
@@ -92,6 +91,7 @@ def registerUDF(sqlContext):
 	#register all user defined functions
 	sqlContext.registerFunction("getBrowser", getBrowser)
 	sqlContext.registerFunction("setColValues", setColValues)
+	sqlContext.registerFunction("getDomainName", getDomainName)
 	
 
 def getCurrentTimeStamp():
@@ -99,30 +99,53 @@ def getCurrentTimeStamp():
 	unixtime = calendar.timegm(d.utctimetuple())
 	return unixtime
 
+def getDateTimeFormat(timestamp):
+	return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
 # cs-uri-stem, c-user-agent
 def setColValues(uri, type):
 	
 	if uri:
 		o = urlparse(uri)
-		pathParams = parse_qs(o.path)
-		queryParams = parse_qs(o.query)
+		pathParams = o.path
+		
+		if o.query is not None:
+			# the log is not proper so ading extra logic, multiple projectsids found
+			param = o.query.split(')%20format',1)[0]
+			queryParams = parse_qs(param)
 		
 		if type in COL_PROJECTID:
 			if PROJECTID in queryParams:
-				return queryParams[PROJECTID]
+				return queryParams[PROJECTID][0]
 		elif type in COL_FONTTYPE or type in COL_FONTID:
 			if pathParams:
-				revfont = re.search('\w+\.\w+/',o.path[::-1])
-				if revfont is not None:
-					font = revfontType[::-1][1:]
+				revfont = re.search(r'(.*\/)(.*)',pathParams)
+				if revfont is not None and revfont.group(2) is not None:
+					font = revfont.group(2)
 					if font:
 						fontSplit = re.split('\.',font)
 						if type in COL_FONTID:
-							return fontSplit[0]
+							if len(fontSplit) > 0:
+								return fontSplit[0]
+							else:
+								return None
 						elif type in COL_FONTTYPE:
-							return fontSplit[1]
+							if len(fontSplit) > 1:
+								return fontSplit[1]
+							else:
+								return None
 		
 	return None
+	
+def getDomainName(uri):
+	
+	if uri:
+		params = urlparse(uri)
+		path = params.path
+		
+		return path
+	else:
+		return None
 	
 		
 def processRdd(rdd):
@@ -130,6 +153,7 @@ def processRdd(rdd):
 	#covnert to a dataframe from rdd
 	sqlContext = SQLContext(rdd.context)
 	registerUDF(sqlContext)
+	print 'Started Processing the streams'
 
 	desiredCol = ['c-ip','cs-uri-stem','c-user-agent','customer-id','x-ec_custom-1']
 	if rdd.count() > 0:
@@ -139,6 +163,9 @@ def processRdd(rdd):
 		#startTime
 		endTime = getCurrentTimeStamp()
 		startTime = endTime - 10
+		
+		endTime = getDateTimeFormat(endTime)
+		startTime = getDateTimeFormat(startTime)
 		df = df.withColumn(COL_STARTTIME, lit(startTime))
 		
 		#endTime
@@ -146,23 +173,25 @@ def processRdd(rdd):
 		
 		df.registerTempTable("tempTable")
 		query = ('select' + 
-				' startTime,' +  																		#startTime
-				' endTime,' +  																			#endTime				
-				' `customer-id` as ' +  COL_CUSTOMERID +  ',' +										#customerid				
+				' startTime,' +  																				#startTime
+				' endTime,' +  																					#endTime				
+				' `customer-id` as ' +  COL_CUSTOMERID +  ',' +													#customerid				
 				' setColValues(`cs-uri-stem`, ' +  '\'' + COL_PROJECTID + '\') as ' +  COL_PROJECTID + ',' +	#projectid					 	
 				' setColValues(`cs-uri-stem`, ' +  '\'' + COL_FONTTYPE + '\') as ' +  COL_FONTTYPE +  ',' + 	#FontType
 				' setColValues(`cs-uri-stem`, ' +  '\'' + COL_FONTID + '\') as ' +  COL_FONTID +  ',' + 		#FontId
-				' `x-ec_custom-1` as ' +  COL_DOMAINNAME +  ',' + 									#DomainName
-				' getBrowser(`c-user-agent`) as ' + COL_USERAGENT +  ',' + 							#UserAgent
-				' `c-ip` as ' +  COL_IPADDRESS + 														#customer ipaddress   
+				' getDomainName(`x-ec_custom-1`) as ' +  COL_DOMAINNAME +  ',' + 								#DomainName
+				' getBrowser(`c-user-agent`) as ' + COL_USERAGENT +  ',' + 										#UserAgent
+				' `c-ip` as ' +  COL_IPADDRESS + 																#customer ipaddress   
 				' from tempTable')
 				
 		
-		print(query)
+		print "query to run : ", query
 		df = sqlContext.sql(query)
-		print df.head(10)
+		print df.head(2)
 		
 		WriteToTable(df, REDSHIFT_PAGEVIEW_TBL)
+	else:
+		print 'Nothing to process'
 				
 if __name__ == "__main__":
 	#_conf = new SparkConf(true)
@@ -181,7 +210,7 @@ if __name__ == "__main__":
 	kinesisStream.foreachRDD(processRdd)
 	
 	ssc.start()
-    	ssc.awaitTermination()
+	ssc.awaitTermination()
 	print "Streaming suspended"
 
 
