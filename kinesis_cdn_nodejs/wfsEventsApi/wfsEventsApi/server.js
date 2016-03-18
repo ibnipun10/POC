ef712dd5a128e9bdf1c9e087021f3b0184ec74e5
nipun agarwal <nipun.agarwal@monotype.com>
2016-03-18 10:55:15 +0530

    update the code

--- kinesis_cdn_nodejs/wfsEventsApi/wfsEventsApi/server.js
+++ kinesis_cdn_nodejs/wfsEventsApi/wfsEventsApi/server.js
@@ -29,7 +29,7 @@ server.route({
             var httpstatus = headers["x-ec-returnstatus"];
             
             if (useragent != null && useragent != undefined)
-                useragent = '\"' + useragent + '\"';
+                useragent = useragent.replace(',','');
             
             var params = [uri, projectid, ip, timestamp, useragent, referrer, httpstatus];
             var paramsCsv = utils.convertTocsv(params);
